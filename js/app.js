8a76b00cc2109451013b44f1b2e38690b18fd23a
Will Moore <w.moore@dundee.ac.uk>
2015-11-28 23:36:41 +0000

    Load Datasets on project click

--- js/app.js
+++ js/app.js
@@ -7,10 +7,13 @@ var ProjectListItem = React.createClass({
 
     render: function() {
         var name = this.props.project.name,
+            childCount = this.props.project.childCount,
             id = this.props.project.id;
         return (
             <li>
-              <button onClick={this.handleClick} value="{id}">{name}</button>
+              <button onClick={this.handleClick} value="{id}">
+                {name} [{childCount}]
+              </button>
             </li>
         );
     }
@@ -18,10 +21,10 @@ var ProjectListItem = React.createClass({
 
 var ProjectList = React.createClass({
 
-    handleProjectClick: function(projectId) {
-        console.log("handleProjectClick", projectId);
-        this.props.handleProjectClick(projectId);
-    },
+    // handleProjectClick: function(projectId) {
+    //     console.log("handleProjectClick", projectId);
+    //     this.props.handleProjectClick(projectId);
+    // },
 
     componentDidMount: function() {
         $.ajax({
@@ -48,20 +51,104 @@ var ProjectList = React.createClass({
         var lastCategory = null;
         this.state.data.forEach(function(project) {
             rows.push(<ProjectListItem
-                        onClick={self.handleProjectClick}
+                        onClick={self.props.handleProjectClick}
                         project={project}
                         key={project.id} />);
         });
         return (
-            <ul>{rows}</ul>
+            <ul style={{float:'left'}}>{rows}</ul>
         );
     }
 });
+
+
+var DatasetList = React.createClass({
+
+    // handleProjectClick: function(projectId) {
+    //     console.log("handleProjectClick", projectId);
+    //     this.props.handleProjectClick(projectId);
+    // },
+
+    // componentDidMount: function() {
+    //     $.ajax({
+    //         url: "http://localhost:4080/webgateway/api/containers/",
+    //         jsonp: "callback",
+    //         dataType: 'jsonp',
+    //         cache: false,
+    //         success: function(data) {
+    //             this.setState({data: data.projects});
+    //         }.bind(this),
+    //         error: function(xhr, status, err) {
+    //             // console.error(this.props.url, status, err.toString());
+    //         }.bind(this)
+    //     });
+    // },
+
+    // getInitialState: function() {
+    //     return {datasets: []};
+    // },
+
+    render: function() {
+        console.log("DatasetList render()", this.props.datasets.length);
+        var rows = [];
+        var self = this;
+        this.props.datasets.forEach(function(dataset) {
+            rows.push(<DatasetListItem
+                        onClick={self.props.handleProjectClick}
+                        dataset={dataset}
+                        key={dataset.id} />);
+        });
+        return (
+            <ul style={{float:'left'}} >{rows}</ul>
+        );
+    }
+});
+
+var DatasetListItem = React.createClass({
+    handleClick: function() {
+        console.log("click dataset", this.props.dataset.id);
+        this.props.onClick(this.props.dataset.id);
+    },
+
+    render: function() {
+        var name = this.props.dataset.name,
+            childCount = this.props.dataset.childCount,
+            id = this.props.dataset.id;
+        return (
+            <li>
+                <button onClick={this.handleClick} value="{id}">
+                    {name} [{childCount}]
+                </button>
+            </li>
+        );
+    }
+});
+
+
 
 var PDIContainer = React.createClass({
 
     handleProjectClick: function(projectId) {
         console.log("PDIContainer", projectId);
+        var data = {'id': projectId};
+        $.ajax({
+            url: "http://localhost:4080/webgateway/api/datasets/",
+            jsonp: "callback",
+            data: data,
+            dataType: 'jsonp',
+            cache: false,
+            success: function(data) {
+                console.log(data);
+                this.setState({datasets: data.datasets});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                // console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+
+    getInitialState: function() {
+        return {datasets: []};
     },
 
     render: function() {
@@ -69,6 +156,7 @@ var PDIContainer = React.createClass({
             <div><h1>PDI</h1>
             <ProjectList 
                 handleProjectClick={this.handleProjectClick} />
+            <DatasetList datasets={this.state.datasets} />
             </div>
         );
     }
